980e6ed54639ed6fe17bba4cc44503f7ec27319e
mell0r1ne <s303330@studenti.polito.it>
2024-01-24 16:50:07 +0100

    Fix /list

--- formatter.js
+++ formatter.js
@@ -1,42 +1,36 @@
 function formatVisitedPlace(place) {
-    let res = "<li>"
-    if(place.URL) res += `<a href="${place.URL}">${place.NAME} ✅</a>`
-    else res += `${place.NAME} ✅`
-    if(place.RATING) res += `<span>   ⭐️ ${place.rating}</span>`
-    if(place.VISITED) res += `<span>   🗓 ${place.VISITED}</span>`
-    res += `</li>`
+    let res = "- "
+    if(place.URL) res += `<a href="${place.URL}">${place.NAME} ✅</a>\n`
+    else res += `${place.NAME} ✅\n`
+    if(place.RATING) res += `   ⭐️ <i>${place.RATING}</i>\n`
+    if(place.VISITED) res += `   🗓 <i>${place.VISITED.split(" ")[0]}</i>\n`
+    res += `\n`
     return res
 }
 
 function formatPlaceToVisit(place) {
-    let res = "<li>"
-    if(place.URL) res += `<a href="${place.URL}">${place.NAME} ❌</a>`
-    else res += `${place.NAME} ❌`
-    res += `<span>   <i>Da provare!</i></span>`
-    res += `</li>`
+    let res = "- "
+    if(place.URL) res += `<a href="${place.URL}">${place.NAME} ❌</a>\n`
+    else res += `${place.NAME} ❌\n`
+    res += `   <i>Da provare!</i>`
+    res += `\n`
     return res
 }
 
-function formattedList(places){
+export function formattedList(places){
     let res = ""
     const toEat = places.filter(place => place.TYPE === "mangiare")
     const toVisit = places.filter(place => place.TYPE === "visitare")
     if(toEat.length > 0){
-        res += "<h2>🍴 Dove mangiare</h2><br/>"
-        res += "<ul>"
+        res += "<b>🍴 Dove mangiare</b>\n\n"
         toEat.forEach(place => res += formatVisitedPlace(place))
-        res += "</ul>"
     }
-    res+= "<br/><br/>"
+    res+= "\n"
     if(toVisit.length > 0){
-        res += "<h2>👀 Cose da vedere</h2><br/>"
-        res += "<ul>"
+        res += "<b>👀 Cose da vedere</b>\n\n"
         toVisit.forEach(place => res += formatPlaceToVisit(place))
-        res += "</ul>"
     }
     return res
 }
 
 
-const formatter = {formattedList}
-export default formatter
--- setupCommands.js
+++ setupCommands.js
@@ -7,7 +7,7 @@ import {
 } from "./index.js";
 import { Keyboard } from "grammy";
 import dayjs from "dayjs";
-import { formatter } from "./formatter.js";
+import { formattedList } from "./formatter.js";
 
 export function setupCommands(bot) {
   const cmd = {
@@ -24,20 +24,21 @@ export function setupCommands(bot) {
     list: "Body count",
   };
 
-	//start
-	bot.command(cmd.start, async (ctx) => {
-		let userId = ctx.from.id;
-		if(monthNumbers[userId] === undefined){
-			monthNumbers[userId] = dayjs().month();
-		}
-		initStatus(userId, 'start');
-		await ctx.reply(
-			"Ciao Zoccola!\nQuesto è il nostro bot per gestire i posti dove andare a battere insieme!\n\nEh? Ancora non hai capito come funziona il bot? 😅 Che puttana...\n\nLancia il comando /faq per vedere i dettagli sul listino prezzi!", {
-				reply_markup: {remove_keyboard: true, selective: true},
-			}
-		);
-		changeStatus(ctx.from.id, "start");
-	});
+  //start
+  bot.command(cmd.start, async (ctx) => {
+    let userId = ctx.from.id;
+    if (monthNumbers[userId] === undefined) {
+      monthNumbers[userId] = dayjs().month();
+    }
+    initStatus(userId, "start");
+    await ctx.reply(
+      "Ciao Zoccola!\nQuesto è il nostro bot per gestire i posti dove andare a battere insieme!\n\nEh? Ancora non hai capito come funziona il bot? 😅 Che puttana...\n\nLancia il comando /faq per vedere i dettagli sul listino prezzi!",
+      {
+        reply_markup: { remove_keyboard: true, selective: true },
+      }
+    );
+    changeStatus(ctx.from.id, "start");
+  });
 
   //faq
   bot.command(cmd.faq, async (ctx) => {
@@ -48,20 +49,68 @@ export function setupCommands(bot) {
     );
   });
 
-	//insert
-	bot.command(cmd.insert, async (ctx) => {
-		initStatus(ctx.from.id, 'insert')
-		let username = ctx.from.username;
-		const keyboard = new Keyboard();
-		keyboard.add(btnMsgs[0], btnMsgs[1]);
-		keyboard.oneTime();
-		keyboard.resize_keyboard = true;
-		keyboard.selective = true
-		await ctx.reply(`Ok, bene, aspetta che ti applaudo per l'idea.\n\nPerchè me lo stai proponendo, @${username}?`, {
-			reply_markup: {...keyboard, force_reply: true},
-		});
-		changeStatus(ctx.from.id, cmd.insert);
-	});
+  //insert
+  bot.command(cmd.insert, async (ctx) => {
+    initStatus(ctx.from.id, "insert");
+    let username = ctx.from.username;
+    const keyboard = new Keyboard();
+    keyboard.add(btnMsgs[0], btnMsgs[1]);
+    keyboard.oneTime();
+    keyboard.resize_keyboard = true;
+    keyboard.selective = true;
+    await ctx.reply(
+      `Ok, bene, aspetta che ti applaudo per l'idea.\n\nPerchè me lo stai proponendo, @${username}?`,
+      {
+        reply_markup: { ...keyboard, force_reply: true },
+      }
+    );
+    changeStatus(ctx.from.id, cmd.insert);
+  });
+
+  //list
+  bot.command(cmd.list, async (ctx) => {
+    let mockPlaces = [
+      {
+        ID: 2,
+        CHATID: 1,
+        USERID: "mell0r1ne",
+        TIMESTAMP: "2024-01-24 11:52:44",
+        NAME: "giappone",
+        VISITED: null,
+        RATING: null,
+        TYPE: "visitare",
+        URL: null,
+      },
+      {
+        ID: 3,
+        CHATID: 1,
+        USERID: "mell0r1ne",
+        TIMESTAMP: "2024-01-24 11:54:03",
+        NAME: "Rock Burger",
+        VISITED: "2024-01-24 12:04:39",
+        RATING: 3.4,
+        TYPE: "mangiare",
+        URL: "http://www.rockburgertorino.it/",
+      },
+      {
+        ID: 3,
+        CHATID: 1,
+        USERID: "mell0r1ne",
+        TIMESTAMP: "2024-01-24 11:54:03",
+        NAME: "La prosciutteria",
+        VISITED: null,
+        RATING: null,
+        TYPE: "mangiare",
+        URL: "http://www.rockburgertorino.it/",
+      },
+    ];
+    await ctx.reply(formattedList(mockPlaces), {
+      parse_mode: "HTML",
+      link_preview_options: {
+        is_disabled: true
+      },
+    });
+  });
 
   //menu creation
   bot.api.setMyCommands(
