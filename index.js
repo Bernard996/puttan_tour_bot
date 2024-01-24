4ee2af1af50b674fa112f40638cceb2d6d3e4a00
Bernard Borio <bernardborio@gmail.com>
2024-01-24 10:42:47 +0100

    Minor fixes and improvements

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Bot, InlineKeyboard, Keyboard } from "grammy";
+import { Bot } from "grammy";
 import { setupCommands } from "./setupCommands.js";
 
 ***REMOVED***
@@ -11,51 +11,59 @@ export const status = {
   selectVisit: false,
 };
 
-const btnMsgs = ["Posto dove mangiare", "Posto da visitare"];
+export const btnMsgs = ["Posto dove mangiare", "Posto da visitare"];
 
-export const keyboard = new Keyboard();
-keyboard.add(btnMsgs[0], btnMsgs[1]);
-keyboard.oneTime();
-keyboard.resize_keyboard = true;
+function main(){
+  setupCommands(bot);
 
-setupCommands(bot);
-
-bot.on("message", async (ctx) => {
-  if (status.start) {
-    await ctx.reply("Dammi un comando, sono la tua schiava!");
-    changeStatus("start");
-  } 
-  else if (status.insert) {
-    if (ctx.message.text !== btnMsgs[0] && ctx.message.text !== btnMsgs[1]) {
-      await ctx.reply(
-        "Hai sbagliato! Devi scegliere la tipologia di posto da inserire!"
-      );
-    } 
-    else {
-      if (ctx.message.text === btnMsgs[0]) {
-        await ctx.reply("Inserisci il nome del posto dove mangiare");
-        changeStatus("selectFood");
-      } else if (ctx.message.text === btnMsgs[1]) {
-        await ctx.reply("Inserisci il nome del posto da visitare");
-        changeStatus("selectVisit");
+  bot.on("message", async (ctx) => {
+    //handle start status
+    if (status.start) {
+      await ctx.reply("Dammi un comando, sono la tua schiava!", {
+        reply_markup: {remove_keyboard: true}
+      });
+      changeStatus("start");
+    }
+    //handle insert status, showing the keyboard
+    else if (status.insert) {
+      //error message with wrong strings
+      if (ctx.message.text !== btnMsgs[0] && ctx.message.text !== btnMsgs[1]) {
+        await ctx.reply(
+          "Hai sbagliato! Devi scegliere la tipologia di posto da inserire!"
+        );
+      }
+      //right case, handling name insert
+      else {
+        if (ctx.message.text === btnMsgs[0]) {
+          await ctx.reply("Inserisci il nome del posto dove mangiare", {
+            reply_markup:{remove_keyboard: true}
+          });
+          changeStatus("selectFood");
+        } else if (ctx.message.text === btnMsgs[1]) {
+          await ctx.reply("Inserisci il nome del posto da visitare", {
+            reply_markup:{remove_keyboard: true}
+          });
+          changeStatus("selectVisit");
+        }
       }
     }
-  }
-  else if (status.selectFood || status.selectVisit) {
-    await ctx.reply("Inserisci l'URL del posto selezionato (opzionale)");
-  }
-});
+    //handle url insert
+    else if (status.selectFood || status.selectVisit) {
+      await ctx.reply("Inserisci l'URL del posto selezionato (opzionale)");
+    }
+  });
 
-//Start the Bot
-bot.start();
+  //Start the Bot
+  bot.start();
+}
+
+
 
 export function changeStatus(cmd) {
-  Object.entries(status).forEach(([key, value]) => {
-    if (key === cmd) {
-      status[key] = true;
-    } else {
-      status[key] = false;
-    }
+  Object.entries(status).forEach(([key, _value]) => {
+    status[key] = key === cmd;
   });
   console.log(status);
 }
+
+main()
--- setupCommands.js
+++ setupCommands.js
@@ -1,47 +1,54 @@
-import { Keyboard } from "grammy";
-import { status, keyboard, changeStatus } from "./index.js";
+import {btnMsgs, changeStatus} from "./index.js";
+import {Keyboard} from "grammy";
 
 export function setupCommands(bot) {
-  const cmd = {
-    start: "start",
-    faq: "faq",
-    insert: "insert",
-  };
+	const cmd = {
+		start: "start",
+		faq: "faq",
+		insert: "insert",
+	};
 
-  const cmdDesc = {
-    start: "Avvia il bot, zoccola",
-    faq: "Faq-ami tutta",
-    insert: "Inserisci un posto",
-  };
+	const cmdDesc = {
+		start: "Avvia il bot, zoccola",
+		faq: "Faq-ami tutta",
+		insert: "Inserisci un posto",
+	};
 
-  //start
-  bot.command(cmd.start, async (ctx) => {
-    await ctx.reply(
-      "Ciao Zoccola! Questo è il nostro bot per gestire i posti dove andare a troieggiare insieme! Ancora non hai capito come funziona il bot? 😅 Sei proprio una puttana... Lancia il comando /faq per vedere i dettagli sui comandi disponibili e smettila di rompere i coglioni a me!"
-    );
-    changeStatus(cmd.start);
-  });
+	//start
+	bot.command(cmd.start, async (ctx) => {
+		await ctx.reply(
+			"Ciao Zoccola! Questo è il nostro bot per gestire i posti dove andare a troieggiare insieme! Ancora non hai capito come funziona il bot? 😅 Sei proprio una puttana... Lancia il comando /faq per vedere i dettagli sui comandi disponibili e smettila di rompere i coglioni a me!", {
+				reply_markup: {remove_keyboard: true},
+			}
+		);
+		changeStatus(cmd.start);
+	});
 
-  //faq
-  bot.command(cmd.faq, async (ctx) => {
-    await ctx.reply(
-      `/${cmd.start}: avvia il bot\n` +
-      `/${cmd.faq}: mostra questo messaggio\n`+
-      `/${cmd.insert}: inserisci un nuovo posto\n`
-    );
-  });
+	//faq
+	bot.command(cmd.faq, async (ctx) => {
+		await ctx.reply(
+			`/${cmd.start}: avvia il bot\n` +
+			`/${cmd.faq}: mostra questo messaggio\n` +
+			`/${cmd.insert}: inserisci un nuovo posto\n`
+		);
+	});
 
-  bot.command(cmd.insert, async (ctx) => {
-    await ctx.reply("Scegli il tipo di posto che vuoi inserire", {
-      reply_markup: keyboard,
-    });
-    changeStatus(cmd.insert);
-  });
+	//insert
+	bot.command(cmd.insert, async (ctx) => {
+		const keyboard = new Keyboard();
+		keyboard.add(btnMsgs[0], btnMsgs[1]);
+		keyboard.oneTime();
+		keyboard.resize_keyboard = true;
+		await ctx.reply("Scegli il tipo di posto che vuoi inserire", {
+			reply_markup: keyboard,
+		});
+		changeStatus(cmd.insert);
+	});
 
-  //menu
-  bot.api.setMyCommands(
-    Object.entries(cmd).map(([key, value]) => {
-      return { command: value, description: cmdDesc[key] };
-    })
-  );
+	//menu creation
+	bot.api.setMyCommands(
+		Object.entries(cmd).map(([key, value]) => {
+			return {command: value, description: cmdDesc[key]};
+		})
+	);
 }
